4f8d0287174e66fa8621b9e6abcadc13c66426c7
ram <ram4256@gmail.com>
2018-02-03 15:22:28 +0530

    new pages implemeted

---
+++ src/com/selenium/pages/LoginPgae.java
@@ -0,0 +1,13 @@
+package com.selenium.pages;
+
+import org.testng.annotations.Test;
+
+public class LoginPgae {
+	
+	@Test
+	public void loginpage()
+	{
+		System.out.println("login page");
+	}
+
+}
---
+++ src/com/selenium/pages/SearchPgae.java
@@ -0,0 +1,13 @@
+package com.selenium.pages;
+
+import org.testng.annotations.Test;
+
+public class SearchPgae {
+	
+	@Test
+	public void searchPage()
+	{
+		System.out.println("search page");
+	}
+
+}
