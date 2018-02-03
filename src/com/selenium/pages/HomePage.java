61b70b63284c69b4c4e566b78703501a04416458
ram <ram4256@gmail.com>
2018-02-03 12:03:08 +0530

    fresh selenium repo

---
+++ src/com/selenium/pages/HomePage.java
@@ -0,0 +1,12 @@
+package com.selenium.pages;
+
+import org.testng.annotations.Test;
+
+public class HomePage {
+
+	@Test
+	public void login()
+	{
+		System.out.println("home page");
+	}
+}
---
+++ src/com/selenium/util/RemoveDUplicates.java
@@ -0,0 +1,12 @@
+package com.selenium.util;
+
+import org.testng.annotations.Test;
+
+public class RemoveDUplicates {
+	
+	@Test
+	public void util()
+	{
+		System.out.println("remove duplicates ");
+	}
+ }
